a1ffb2841de64e1de1487dccb09eb83dfeae4ab4
Gustavo Rondello <gustavorondello99@outlook.com>
2022-02-23 19:31:16 -0300

    Requisito 5 feito metade

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Form from './components/Form';
 class App extends React.Component {
   constructor() {
     super();
+
+    this.handleButton = this.handleButton.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
+
     this.state = {
       cardName: '',
       cardDescription: '',
@@ -12,26 +17,43 @@ class App extends React.Component {
       cardAttr2: '',
       cardAttr3: '',
       cardImage: '',
-      cardRare: '',
-      cardTrunfo: true,
+      cardRare: 'normal',
+      cardTrunfo: false,
       hasTrunfo: false,
-      isSaveButtonDisabled: false,
+      isSaveButtonDisabled: true,
       onInputChange: () => {},
       onSaveButtonClick: () => {},
     };
   }
 
-  onInputChange = ({ target }) => {
+  handleButton() {
+    const values = ['cardName',
+      'cardDescription',
+      'cardAttr1',
+      'cardAttr2',
+      'cardAttr3',
+      'cardImage',
+      'cardRare'];
+    const isTrue = values.every((nome) => {
+      const estados = this.state;
+      return estados[nome];
+    });
+    this.setState({
+      isSaveButtonDisabled: !isTrue,
+    }, () => console.log(this.state.isSaveButtonDisabled));
+  }
+
+  onInputChange({ target }) {
     const { name } = target;
     console.log(name);
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
-    });
+    }, () => this.handleButton());
   }
 
-  onSaveButtonClick = () => {
-    console.log(this.state);
+  onSaveButtonClick() {
+    console.log('sei la');
   }
 
   render() {
@@ -42,11 +64,23 @@ class App extends React.Component {
       cardAttr3,
       cardImage,
       cardRare,
-      cardTrunfo } = this.state;
+      cardTrunfo,
+      isSaveButtonDisabled } = this.state;
     return (
       <div>
         <h1>Tryunfo Iniciando Projeto</h1>
-        <Form value={ this.state } onInputChange={ this.onInputChange } />
+        <Form
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+          onInputChange={ this.onInputChange }
+        />
         <Card
           cardName={ cardName }
           cardDescription={ cardDescription }
