13396a96b05edd7b25ff843a62d8a1b40717b2c0
Gustavo Rondello <gustavorondello99@outlook.com>
2022-02-23 14:54:10 -0300

    Requisito 1 completo

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Input from './Inputs';
+
+class Form extends Component {
+  render() {
+    return (
+      <form>
+        <Input />
+      </form>
+    );
+  }
+}
+
+export default Form;
